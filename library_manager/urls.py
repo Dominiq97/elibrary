71e8dedee38f5c9eb6a836407ac33894d2df74eb
Dominiq97 <catanadominic@gmail.com>
2022-11-22 18:26:33 +0200

    end first endpoints

--- library_manager/urls.py
+++ library_manager/urls.py
@@ -4,21 +4,9 @@ from library_manager import views
 from library_manager import views # LibraryViewset #GetBookViewset#, BookViewset
 
 router = DefaultRouter()
-# router1 = DefaultRouter()
-# router.register(r'', LibraryViewset, basename='books')
-# router1.register(r'', GetBookViewset, basename='books')
-# router.register(r'books', BookViewset, basename='book')
+# router.register(r'', views.LibraryViewset, basename='books')
 
 urlpatterns = [
     path('api/v1/books/', views.books_list),
     path('api/v1/books/<str:title>', views.books_list_title),
-    # path('api/v1/books/<int:id>', views.delete),
-    # path('api/v1/books/add', views.post),
-    # path('api/v1/books/<str:author>', views.books_list_author),
-    # path('api/v1/books/<int:publisher>', views.books_list_publisher),
-    # path('api/v1/books/add', views.add_book),
-    # path('books/', views.books_list, name="books"),
-    # path('books/<str:title>', views.book_detail, name="books"),
-    
-    # path('api/v1/books/<str:title>', views.book_detail, name="book_detail"),
 ]
--- library_manager/views.py
+++ library_manager/views.py
@@ -38,13 +38,9 @@ def books_list(request):
         books_serializer = BookSerializer(books, many=True)
         return JsonResponse(books_serializer.data, safe=False) 
 
-
-
-
 @api_view(['GET'])
 def books_list_title(request, title):
     if request.method == 'GET':
-    #     book = Book.objects.get(title = title)
         if title is not None:
             books = Book.objects.filter(title=title)
         
@@ -61,90 +57,9 @@ def post(self, request):
         return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['DELETE'])
-def delete(self, request, id=None):
+def delete(self, request, id):
     book = get_object_or_404(Book, id=id)
     book.delete()
     return Response({"status": "success", "data": "Car Deleted"})
-
-# @api_view(['GET'])
-# def books_list_publisher(request, publisher):
-#     if request.method == 'GET':
-#     #     book = Book.objects.get(publisher = publisher)
-#         if publisher is not None:
-#             books = Book.objects.filter(publisher=int(publisher))
-        
-#         books_serializer = BookSerializer(books, many=True)
-#         return JsonResponse(books_serializer.data, safe=False)    # 'safe=False' for objects serialization
-
-
-# @api_view(['GET'])
-# def books_list_author(request, author):
-#     # if request.method == 'GET':
-#     #     book = Book.objects.get(author = author)
-#     if author is not None:
-#         books = Book.objects.filter(author=author)
-    
-#     books_serializer = BookSerializer(books, many=True)
-#     return JsonResponse(books_serializer.data, safe=False) 
-
-
-# @api_view(['GET'])
-# def books_list_publisher(request, publisher):
-#     # if request.method == 'GET':
-#     #     book = Book.objects.get(title = title)
-#     if publisher is not None:
-#         books = Book.objects.filter(publisher=publisher)
-    
-#     books_serializer = BookSerializer(books, many=True)
-#     return JsonResponse(books_serializer.data, safe=False) 
-
-# @api_view(['POST'])
-# def add_book(request):
-#     serializer = BookSerializer(data=request.data)
-#     if serializer.is_valid():
-#         serializer.save()
-#         return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
-#     else:
-#         return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-
-
-
-
-# @api_view(['GET'])
-# def books_list(request):
-#     if request.method == 'GET':
-#         books = Book.objects.all()
-#         books_serializer = BookSerializer(books, many=True)
-#         return JsonResponse(books_serializer.data, safe=False) 
-
-# class GetBookViewset(viewsets.ModelViewSet):
-#     serializer_class = BookSerializer
-
-#     def get_queryset(self):
-#         title = self.request.query_params.get('title')
-#         book = Book.objects.get(id=title)
-#         return book.showroom
-
-#     @swagger_auto_schema(
-#         operation_description="give title to get library details",
-#         manual_parameters=[title],
-#         responses={200: FETCH_BOOK_SUCCESS}
-#     )
-
-#     def list(self, request, *args, **kwargs):
-#         queryset = self.get_queryset()
-#         serializer = BookSerializer(queryset)
-#         return Response(serializer.data)
-
-# def book_detail(request, title):
-#     try: 
-#         book = Book.objects.get(title=title) 
-#     except Book.DoesNotExist: 
-#         return JsonResponse({'message': 'The book does not exist'}, status=status.HTTP_404_NOT_FOUND) 
-#     if request.method == 'GET': 
-#         book_serializer = BookSerializer(book) 
-#         return JsonResponse(book_serializer.data) 
-
-
 
 
