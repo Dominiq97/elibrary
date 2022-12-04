7a884c78de8857694a12117dccd14099523a30c8
Dominiq97 <catanadominic@gmail.com>
2022-12-04 23:35:50 +0200

    delete unwanted

--- customer/permissions.py
+++ customer/permissions.py
@@ -5,8 +5,6 @@ from customer.models import Customer
 class CustomersPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         for i in Customer.objects.all():
-            print(request.user)
-            print(i)
             if str(request.user) == str(i.first_name+" "+i.last_name):
                 return True
         return False
